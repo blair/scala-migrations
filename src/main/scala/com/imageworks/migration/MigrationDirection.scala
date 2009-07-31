f1f20b953ccbeb867357386cdbcb7c3207871797
Blair Zajac <blair@imageworks.com>
2009-07-31 16:21:51 -0700

    Clean up MigrationDirection and its subclasses.
    * src/main/scala/com/imageworks/migration/MigrationDirection.scala
      (MigrationDirection):
        Add documentation.
      (MigrationDirection#str):
        Declare as an abstract val instead of an abstract def.
      (Up),
      (Down):
        Add the override modifier to the 'str' val.
        Add documentation.

--- src/main/scala/com/imageworks/migration/MigrationDirection.scala
+++ src/main/scala/com/imageworks/migration/MigrationDirection.scala
@@ -32,20 +32,34 @@
  */
 package com.imageworks.migration
 
+/**
+ * Sealed abstract class that defines the direction to run a
+ * migration.
+ */
 sealed abstract class MigrationDirection
 {
   /**
    * A human readable string representing the migration direction.
    */
-  def str : String
+  val str : String
 }
 
-case object Up extends MigrationDirection
+/**
+ * Case object used to indicate that a migration should be installed.
+ */
+case object Up
+  extends MigrationDirection
 {
+  override
   val str = "up"
 }
 
-case object Down extends MigrationDirection
+/**
+ * Case object used to indicate that a migration should be removed.
+ */
+case object Down
+  extends MigrationDirection
 {
+  override
   val str = "down"
 }
