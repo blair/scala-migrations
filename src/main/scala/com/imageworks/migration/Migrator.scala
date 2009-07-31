fdaec8e5e14bd40a104af2a2f7d32a64f9bb6f7e
Blair Zajac <blair@imageworks.com>
2009-07-30 23:19:47 -0700

    Rename Migrator#with_connections() to Migrator#withConnections().
    * src/main/scala/com/imageworks/migration/Migrator.scala
      (withConnections):
        Renamed from with_connections.
      (run_migration):
        Use the new method name.

--- src/main/scala/com/imageworks/migration/Migrator.scala
+++ src/main/scala/com/imageworks/migration/Migrator.scala
@@ -355,27 +355,27 @@ class RawAndLoggingConnections(val raw : java.sql.Connection,
 /**
  * This sealed trait's specifies the commit behavior on a database
  * connection and its subobjects are used as arguments to the
- * Migrator#with_*connection() methods.  The subobjects specify if a
+ * Migrator#with*Connection() methods.  The subobjects specify if a
  * new connection's auto-commit mode should be left on or disabled.
  * For connections with auto-commit mode disabled it specifies if the
  * current transaction should be rolled back or committed if the
- * closure passed to Migrator#with_*connection() throws an exception.
+ * closure passed to Migrator#with*Connection() throws an exception.
  */
 private sealed trait CommitBehavior
 
 /**
  * The new database connection's auto-commit mode is left on.  Because
  * the connection is in auto-commit mode the
- * Migrator#with_*connection() methods do not commit nor roll back the
+ * Migrator#with*Connection() methods do not commit nor roll back the
  * transaction any before returning the result of the
- * Migrator#with_*connection()'s closure or rethrowing its exception.
+ * Migrator#with*Connection()'s closure or rethrowing its exception.
  */
 private case object AutoCommit
   extends CommitBehavior
 
 /**
  * The new database connection's auto-commit mode is turned off.
- * Regardless if the closure passed to Migrator#with_*connection()
+ * Regardless if the closure passed to Migrator#with*Connection()
  * returns or throws an exception the transaction is committed.
  */
 private case object CommitUponReturnAndException
@@ -383,7 +383,7 @@ private case object CommitUponReturnAndException
 
 /**
  * The new database connection's auto-commit mode is turned off.  If
- * the closure passed to the Migrator#with_*connection() returns
+ * the closure passed to the Migrator#with*Connection() returns
  * normally then transaction is committed; if it throws an exception
  * then the transaction is rolled back.
  */
@@ -598,7 +598,7 @@ class Migrator private (jdbc_conn : Either[DataSource, String],
    *        pair of related connections
    * @return what f returns
    */
-  private[migration] def with_connections[T]
+  private[migration] def withConnections[T]
     (commit_behavior : CommitBehavior)
     (f : RawAndLoggingConnections => T) : T =
   {
@@ -652,7 +652,7 @@ class Migrator private (jdbc_conn : Either[DataSource, String],
                 migration_class.getName)
 
     val migration = migration_class.getConstructor().newInstance()
-    with_connections(AutoCommit) { connections =>
+    withConnections(AutoCommit) { connections =>
       migration.connection_ = connections.logging
       migration.raw_connection_ = connections.raw
       migration.adapter_ = adapter
