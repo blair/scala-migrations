8351c4a72dbb9b0ad0761e72e18d00ce51a5365b
Blair Zajac <blair@imageworks.com>
2012-11-24 21:41:21 -0800

    Document the Unicode case object for different databases.
    Change-Id: I793f77634710e24de166d5fe75c835973c899102

--- src/main/scala/com/imageworks/migration/CharacterSetName.scala
+++ src/main/scala/com/imageworks/migration/CharacterSetName.scala
@@ -38,7 +38,17 @@ package com.imageworks.migration
 sealed trait CharacterSetName
 
 /**
- * The database column should be encoded using Unicode.
+ * The character data types should be encoded using Unicode.
+ *
+ * On Derby, character data types are encoded using Unicode by
+ * default so specifying this does not change the encoding.
+ *
+ * On Oracle, this specifies that NCHAR is used for CharType and
+ * NVARCHAR2 for VarcharType.
+ *
+ * On PostgreSQL, this case object is ignored as the character set
+ * encoding can only be specified when the database is created; using
+ * it generates a runtime warning.
  */
 case object Unicode
   extends CharacterSetName
